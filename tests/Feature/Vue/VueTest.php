abb75fb7e6f696f65a76700570273002fe4706c1
Gerald Buttinger <gerald.buttinger@nic.at>
2018-10-19 12:27:32 +0200

    fix VueTest

--- tests/Feature/Vue/VueTest.php
+++ tests/Feature/Vue/VueTest.php
@@ -15,7 +15,7 @@ class VueTest extends TestCase
     {
         \Form::open('myFormId');
 
-        $this->assertEquals(
+        $this->assertHtmlEquals(
             '<div data-field-wrapper="1" class="form-group" v-bind:class="{ \'has-error\': fields.text.errors.length }"><label for="myFormId_text">Text<sup v-if="fields.text.isRequired">*</sup></label><div role="alert" data-error-container="1" class="alert m-b-1 alert-danger" id="myFormId_text_errors" data-displays-errors-for="text" hidden style="display:none"></div><input type="text" name="text" class="form-control" id="myFormId_text" placeholder="Text" v-model="fields.text.value" v-bind="{ required: fields.text.isRequired, disabled: fields.text.isDisabled }" aria-describedby="myFormId_text_errors" /></div>',
             \Form::text('text')->generate()
         );
