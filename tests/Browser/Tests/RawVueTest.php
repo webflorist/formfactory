0a3fbc742a78646091b0d6b9dc560b69208b360b
Gerald Buttinger <gerald@code.florist>
2019-07-08 15:05:19 +0200

    fix bot-protection tests not running with vue-tests

--- tests/Browser/Tests/RawVueTest.php
+++ tests/Browser/Tests/RawVueTest.php
@@ -2,6 +2,9 @@
 
 namespace FormFactoryTests\Browser\Tests;
 
+use FormFactoryTests\Browser\Tests\Traits\CaptchaTestTrait;
+use FormFactoryTests\Browser\Tests\Traits\HoneypotTestTrait;
+use FormFactoryTests\Browser\Tests\Traits\TimeLimitTestTrait;
 use FormFactoryTests\Browser\Tests\Traits\VueTestTrait;
 
 class RawVueTest extends RawTest
@@ -9,5 +12,8 @@ class RawVueTest extends RawTest
     protected $vueEnabled = true;
     protected $openVueForm = true;
 
-    use VueTestTrait;
+    use VueTestTrait,
+        CaptchaTestTrait,
+        HoneypotTestTrait,
+        TimeLimitTestTrait;
 }
