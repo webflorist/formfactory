d2663fae7c93ac5217e4bbbf359614c578aa4d12
Gerald Buttinger <gerald@code.florist>
2019-09-30 00:26:39 +0200

    remove superfluous function

--- src/Components/FormControls/ImageFileInput.php
+++ src/Components/FormControls/ImageFileInput.php
@@ -7,18 +7,4 @@ use Intervention\Image\Image;
 class ImageFileInput extends FileInput
 {
 
-    /**
-     * Gets called after applying decorators.
-     * Overwrite to perform manipulations.
-     */
-    protected function afterDecoration()
-    {
-        parent::afterDecoration();
-
-        if ($this->attributes->isSet('value')) {
-            if (is_object($this->attributes->value) && is_a($this->attributes->value, Image::class)) {
-                $this->value((string)$this->attributes->value->encode('data-url'));
-            }
-        }
-    }
 }
