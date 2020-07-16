23c43dc8fd1457b0955ffc3b76481bac0a606814
Gerald Buttinger <gerald@code.florist>
2020-07-16 10:35:31 +0200

    FileUpload Storage test

--- src/Utilities/FileUploadManager.php
+++ src/Utilities/FileUploadManager.php
@@ -4,6 +4,7 @@ namespace Webflorist\FormFactory\Utilities;
 
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class FileUploadManager
@@ -11,25 +12,28 @@ class FileUploadManager
 
     public static function storeFile($file, string $formId, string $fieldName)
     {
-        $fileId = Str::random();
-        $key = self::getFileCacheKey($fileId, $formId, $fieldName);
-        cache()->put($key, self::processFile($file), now()->addMinutes(30));
-        return $fileId;
-    }
-
-    private static function processFile($file) {
+        // Make sure, we deal with a UploadedFile.
         if (is_a($file, \Illuminate\Http\Testing\File::class)) {
             /** @var \Illuminate\Http\Testing\File $file */
             $file = new UploadedFile($file->getPathname(), $file->getClientOriginalName(), $file->getClientMimeType(), null, $test = true);
         }
         /** @var UploadedFile $file */
-        $tmpFile = tempnam(sys_get_temp_dir(), '/form_factory_');
-        rename($file->getPathname(), $tmpFile);
-        return [
-            'path' => $tmpFile,
-            'originalName' => $file->getClientOriginalName(),
-            'mimeType' => $file->getClientMimeType()
-        ];
+
+        $fileId = Str::random();
+
+        $file->storeAs(self::getStoragePath(), $fileId);
+
+        cache()->put(
+            self::getFileCacheKey($fileId, $formId, $fieldName),
+            [
+                'fileId' => $fileId,
+                'originalName' => $file->getClientOriginalName(),
+                'mimeType' => $file->getClientMimeType()
+            ],
+            now()->addMinutes(30)
+        );
+
+        return $fileId;
     }
 
     public static function retrieveFile(string $fileId, string $formId, string $fieldName)
@@ -39,7 +43,15 @@ class FileUploadManager
             throw new HttpResponseException(response("Uploaded File not found.", 408));
         }
         $storedFileData = cache()->get($key);
-        return new UploadedFile($storedFileData['path'], $storedFileData['originalName'], $storedFileData['mimeType'], null, true);
+
+        //Re-copy file from storage to tmp.
+        $tmpFile = tempnam(sys_get_temp_dir(), '/form_factory_');
+        file_put_contents($tmpFile, Storage::get(self::getStoragePath() . '/' . $fileId));
+
+        //Remove file from storage
+        Storage::delete(self::getStoragePath() . '/' . $fileId);
+
+        return new UploadedFile($tmpFile, $storedFileData['originalName'], $storedFileData['mimeType'], null, true);
     }
 
     /**
@@ -52,6 +64,14 @@ class FileUploadManager
         $fieldName = FormFactoryTools::convertArrayFieldHtmlName2DotNotation($fieldName);
         $sessionId = session()->getId();
         return "formfactory.$sessionId.$formId.uploaded-files.$fieldName.$fileId";
+    }
+
+    /**
+     * @return string
+     */
+    private static function getStoragePath(): string
+    {
+        return 'temp/form_factory_uploads';
     }
 
 }
